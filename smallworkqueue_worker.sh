a4810e620a62330b9c700d394931290bf0b00f13
Lars Gottesbueren <lars.gottesbueren@kit.edu>
2020-03-17 14:35:55 +0100

    load more appropriate python module

--- smallworkqueue_worker.sh
+++ smallworkqueue_worker.sh
@@ -6,7 +6,7 @@ FAILED_QUEUE_FILE=$QUEUE_FILE".failed"
 DEBUG_FILE=$QUEUE_FILE".debug"
 
 module load compiler/gnu/9.2
-devel/python/3.8.1_gnu_9.2
+module load devel/python/3.7.4_gnu_9.2
 
 MAX_IDLE_STEPS=100
 STEP=0
