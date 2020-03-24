1ad017b3730cd626f41c7e62ec2028b71ec62509
Tobias Heuer <tobias.heuer@gmx.net>
2020-03-24 13:35:13 +0100

    task crunsher works on global queue now

--- mpi/task_crunsher.cc
+++ mpi/task_crunsher.cc
@@ -48,6 +48,28 @@ void write_jobs(const std::string& filename, const std::vector<std::string>& job
   job_stream.close();
 }
 
+void distribute_jobs(const std::string& queue_file, const int num_tasks) {
+  std::vector<std::string> jobs = read_jobs(queue_file);
+  std::random_shuffle(jobs.begin(), jobs.end());
+
+  size_t num_jobs = jobs.size();
+  size_t steps = std::max(1UL, num_jobs / num_tasks + ( num_jobs % num_tasks != 0 ));
+  for ( int task = 0; task < num_tasks; ++task ) {
+    const size_t start = task * steps;
+    const size_t end = std::min(num_jobs, (task + 1) * steps);
+    if ( start >= end || start >= num_jobs ) {
+      break;
+    }
+
+    std::string local_queue_file = queue_file + "." + std::to_string(task);
+    std::ofstream local_stream(local_queue_file);
+    for ( size_t pos = start; pos < end; ++pos ) {
+      local_stream << jobs[pos] << std::endl;
+    }
+    local_stream.close();
+  }
+}
+
 int main(int argc, char** argv) {
     if ( argc != 2 ) {
       std::exit(-1);
@@ -57,11 +79,12 @@ int main(int argc, char** argv) {
     MPI_Init(&argc, &argv);
 
     // Get the rank of the process
-    int rank;
+    int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     std::string queue_file(argv[1]);
-    queue_file = queue_file + "." + std::to_string(rank);
+    std::string local_queue_file = queue_file + "." + std::to_string(rank);
 
     // Check if queue file exists
     if ( !file_exists(queue_file) ) {
@@ -69,20 +92,39 @@ int main(int argc, char** argv) {
     }
 
     size_t steps = 0;
+    std::vector<std::string> failed_jobs;
+    bool waiting_for_new_jobs = true;
     while ( steps < MAX_IDLE_STEPS ) {
-      if ( is_file_empty(queue_file) ) {
-        std::cout << "No jobs found in file '" << queue_file << "'. "
-                  << "Task terminates in " << (MAX_IDLE_STEPS - steps) * SLEEP_TIME
-                  << "ms." << std::endl;
+      if ( !file_exists(local_queue_file) || is_file_empty(local_queue_file) ) {
+        MPI_Barrier(MPI_COMM_WORLD);
+        // At this point all MPI tasks are idle
+
+        if ( rank == 0 ) {
+          // No tasks are running any more try to distribute work
+          if ( file_exists(queue_file) && !is_file_empty(queue_file) ) {
+            if ( waiting_for_new_jobs ) {
+              // Global queue file exists => distribute work to local queues
+              distribute_jobs(queue_file, size);
+              waiting_for_new_jobs = false;
+            } else {
+              // Current queue is finished clear file and wait for new jobs
+              create_file(queue_file);
+              waiting_for_new_jobs = true;
+            }
+          } else {
+            // No work in global queue => wait for new jobs
+            ++steps;
+            std::cout << "No jobs found in file '" << queue_file << "'. "
+                      << "Task terminates in " << (MAX_IDLE_STEPS - steps) * SLEEP_TIME
+                      << "ms." << std::endl;
+          }
+        }
+
         std::this_thread::sleep_for(CHRONO_SLEEP_TIME);
-        ++steps;
+        MPI_Bcast(&steps, 1, MPI_UNSIGNED_LONG, 0, MPI_COMM_WORLD);
       } else {
         steps = 0;
-        std::vector<std::string> jobs = read_jobs(queue_file);
-        std::vector<std::string> failed_jobs;
-
-        // Just for fun
-        std::random_shuffle(jobs.begin(), jobs.end());
+        std::vector<std::string> jobs = read_jobs(local_queue_file);
 
         while ( !jobs.empty() ) {
           std::string job = jobs.back();
@@ -94,9 +136,9 @@ int main(int argc, char** argv) {
 
           if ( ret != 0 ) {
             failed_jobs.push_back(job);
-            write_jobs(queue_file + ".failed", failed_jobs);
+            write_jobs(local_queue_file + ".failed", failed_jobs);
           }
-          write_jobs(queue_file, jobs);
+          write_jobs(local_queue_file, jobs);
         }
       }
     }
